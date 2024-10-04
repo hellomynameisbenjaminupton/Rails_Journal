c458a94d7786f0d0ed4a51b104b5df1cd08cc2c5
BENJAMIN UPTON <ben@thebigredgroup.com.au>
2024-10-04 22:25:12 +1000

    add profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+  
   def home
   end
 
   def about
+  end
+
+  def profile
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'profile', to: 'pages#profile'
 
   devise_for :users
 
