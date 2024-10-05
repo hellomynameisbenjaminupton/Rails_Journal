49565fa722b6862c3f8a778961c945ab80c6fec1
BENJAMIN UPTON <ben@thebigredgroup.com.au>
2024-10-05 11:17:46 +1000

    work avatar upload view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,5 @@
-class UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters
-
-  def show
-    @user = User.find(params[:id])
-  end
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -12,5 +7,4 @@ class UsersController < ApplicationController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :avatar])
   end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :journals, dependent: :destroy
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
